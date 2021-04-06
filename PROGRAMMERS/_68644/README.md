b3ab213c2f0580b155dd0e9a6550292740a2be2a
Bogeunsss <bogun11904@gmail.com>
2021-04-07 01:03:20 +0900

    두 개 뽑아서 더하기

---
+++ PROGRAMMERS/_68644/README.md
@@ -0,0 +1,3 @@
+# Problems
+
+- 68644 | 두 개 뽑아서 더하기 (https://programmers.co.kr/learn/courses/30/lessons/68644?language=java)
---
+++ PROGRAMMERS/_68644/Solution.java
@@ -0,0 +1,28 @@
+package PROGRAMMERS._68644;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Solution {
+	public static int[] solution(int[] numbers) {
+		int[] answer;
+		ArrayList<Integer> sum = new ArrayList<Integer>();
+		
+		for(int i=0; i<numbers.length-1; i++) {
+			for(int j=i+1; j<numbers.length; j++) {
+				if(!sum.contains(numbers[i] + numbers[j])) {
+					sum.add(numbers[i] + numbers[j]);
+				}
+			}
+		}
+		
+		sum.sort(Comparator.naturalOrder());
+		answer = new int[sum.size()];
+		for(int i=0;i<sum.size(); i++) {
+			answer[i] = sum.get(i);
+		}
+        
+        return answer;
+    }
+
+}
