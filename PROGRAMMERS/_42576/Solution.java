185a0c587adc48172c55557baa70104e8c6d0b29
Bogeunsss <bogun11904@gmail.com>
2021-04-07 02:13:34 +0900

    완주하지 못한 선수

---
+++ PROGRAMMERS/_42576/Solution.java
@@ -0,0 +1,18 @@
+package PROGRAMMERS._42576;
+
+import java.util.HashMap;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        int hashSum = 0;
+        HashMap<Integer, String> player = new HashMap<Integer, String>();
+        for(String p : participant) {
+            player.put(p.hashCode(), p);
+            hashSum += p.hashCode();
+        }
+        for(String c : completion) {
+            hashSum -= c.hashCode();
+        }
+        return player.get(hashSum);
+    }
+}
