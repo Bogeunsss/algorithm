292c0ee0514dc2b412f8f824ffc228c7c36584ce
Bogeunsss <bogun11904@gmail.com>
2021-04-06 23:33:47 +0900

    최댓값

---
+++ BEAKJOON/BOJ_2562.java
@@ -0,0 +1,33 @@
+package BEAKJOON;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_2562 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int max = Integer.parseInt(br.readLine());
+		int index = 0;
+		
+		for(int i=1;i<9;i++) {
+			int val = Integer.parseInt(br.readLine());	
+			if(val > max) {
+				max = val;
+				index = i;
+			}
+		}
+		bw.write(Integer.toString(max));
+		bw.newLine();
+		bw.write(Integer.toString(index+1));
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
