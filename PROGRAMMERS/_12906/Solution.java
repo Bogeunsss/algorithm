e3178cdfe983774069335037a82ed31a165367ed
Bogeunsss <bogun11904@gmail.com>
2021-04-07 22:31:20 +0900

    같은 숫자는 싫어

--- PROGRAMMERS/_12906/Solution.java
+++ PROGRAMMERS/_12906/Solution.java
@@ -3,7 +3,7 @@ package PROGRAMMERS._12906;
 import java.util.*;
 
 public class Solution {
-    public static ArrayList<Integer> solution(int []arr) {
+    public ArrayList<Integer> solution(int []arr) {
         int pos;
         ArrayList<Integer> answer = new ArrayList<Integer>();
         for(pos=0; pos<arr.length;) {
@@ -16,11 +16,5 @@ public class Solution {
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {1, 1, 3, 3, 0, 1, 1};
-
-        System.out.println(solution(arr));
     }
 }
