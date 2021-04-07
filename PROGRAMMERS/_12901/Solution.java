8a87d85b09890184b9d2e3bf022a443e141102f6
Bogeunsss <bogun11904@gmail.com>
2021-04-07 21:06:01 +0900

    2016년

---
+++ PROGRAMMERS/_12901/Solution.java
@@ -0,0 +1,16 @@
+package PROGRAMMERS._12901;
+
+import java.util.Arrays;
+
+public class Solution {
+    String[] weeks = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+    int[] months = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public String solution(int a, int b) {
+        int days = 0;
+        for(int i=0; i<a-1; i++) days += months[i];
+        days += b;
+
+        return weeks[days % 7];
+    }
+}
