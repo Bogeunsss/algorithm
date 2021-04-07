47d8db59290ee285e40b5fa9d852dedfc280609b
Bogeunsss <bogun11904@gmail.com>
2021-04-07 20:45:10 +0900

    K번째수

--- PROGRAMMERS/_42748/Solution.java
+++ PROGRAMMERS/_42748/Solution.java
@@ -3,7 +3,7 @@ package PROGRAMMERS._42748;
 import java.util.Arrays;
 
 public class Solution {
-    public static int[] solution(int[] array, int[][] commands) {
+    public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         int index = 0;
 
@@ -14,12 +14,5 @@ public class Solution {
             answer[index++] = temp[k];
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        int[] array = {1, 5, 2, 6, 3, 7, 4};
-        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-
-        System.out.println(Arrays.toString(solution(array, commands)));
     }
 }
