68f12101fb1e45693a1fa58aee00fda5ef4026a2
Bogeunsss <bogun11904@gmail.com>
2021-04-08 17:31:35 +0900

    폰켓몬

---
+++ PROGRAMMERS/_1845/README.md
@@ -0,0 +1,3 @@
+# Problems
+
+- 1845| 폰켓몬 (https://programmers.co.kr/learn/courses/30/lessons/1845?language=java)
---
+++ PROGRAMMERS/_1845/Solution.java
@@ -0,0 +1,12 @@
+package PROGRAMMERS._1845;
+
+import java.util.*;
+
+public class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int num : nums) set.add(num);
+
+        return Math.min(set.size(), nums.length/2);
+    }
+}
