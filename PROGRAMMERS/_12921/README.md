d46398fe84ed5e9b855b6f054bc953f1ebdc2e36
Bogeunsss <bogun11904@gmail.com>
2021-04-11 00:30:36 +0900

    소수 찾기

---
+++ PROGRAMMERS/_12921/README.md
@@ -0,0 +1,3 @@
+# Problems
+
+- 12921| 소수 찾기 (https://programmers.co.kr/learn/courses/30/lessons/12921?language=java)
---
+++ PROGRAMMERS/_12921/Solution.java
@@ -0,0 +1,20 @@
+package PROGRAMMERS._12921;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean[] primes = new boolean[n+1];
+
+        primes[0] = primes[1] = true;
+        for(int i=2; i*i<=n; i++) {
+            if(!primes[i]) {
+                for(int j=i*i; j<=n; j+=i) primes[j] = true;
+            }
+        }
+        for(int i=2; i<=n; i++) {
+            if(!primes[i]) answer++;
+        }
+
+        return answer;
+    }
+}
