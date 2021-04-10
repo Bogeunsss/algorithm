d9f4ee452f50c277a30bc9311d22ee3f04e0e500
Bogeunsss <bogun11904@gmail.com>
2021-04-10 23:03:11 +0900

    문자열 내 p와 y의 개수

---
+++ PROGRAMMERS/_12916/README.md
@@ -0,0 +1,3 @@
+# Problems
+
+- 12916| 문자열 내 p와 y의 개수 (https://programmers.co.kr/learn/courses/30/lessons/12916?language=java)
---
+++ PROGRAMMERS/_12916/Solution.java
@@ -0,0 +1,15 @@
+package PROGRAMMERS._12916;
+
+public class Solution {
+    boolean solution(String s) {
+        s = s.toLowerCase();
+        char[] py = new char[2];
+
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == 'p') py[0]++;
+            else if(s.charAt(i) == 'y') py[1]++;
+        }
+
+        return py[0] == py[1];
+    }
+}
