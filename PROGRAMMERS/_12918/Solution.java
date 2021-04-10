6f403f5531d806e85fa1296435801e4d88f7f861
Bogeunsss <bogun11904@gmail.com>
2021-04-11 00:05:05 +0900

    문자열 다루기 기본

---
+++ PROGRAMMERS/_12918/Solution.java
@@ -0,0 +1,22 @@
+package PROGRAMMERS._12918;
+
+public class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() == 4 || s.length() == 6) {
+            for(int i=0; i<s.length(); i++) {
+                if(!Character.isDigit(s.charAt(i))) {
+                    answer = false;
+                    break;
+                }
+            }
+        }else {
+            answer = false;
+        }
+        return answer;
+    }
+//    정규표현식
+//    public static boolean solution(String s) {
+//        return s.matches("^[0-9]{4}|[0-9]{6}$");
+//    }
+}
