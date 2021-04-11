6d84c2b241552a4e3f8dc957b9656b2e2db36f13
Bogeunsss <bogun11904@gmail.com>
2021-04-11 21:28:01 +0900

    자연수 뒤집어 배열로 만들기

---
+++ PROGRAMMERS/_12932/README.md
@@ -0,0 +1,3 @@
+# Problems
+
+- [12932](https://programmers.co.kr/learn/courses/30/lessons/12932?language=java) | 자연수 뒤집어 배열로 만들기
---
+++ PROGRAMMERS/_12932/Solution.java
@@ -0,0 +1,14 @@
+package PROGRAMMERS._12932;
+
+public class Solution {
+    public int[] solution(long n) {
+        int[] answer = new int[Long.toString(n).length()];
+        int index = 0;
+
+        while(n > 0) {
+            answer[index++] = (int) (n % 10);
+            n /= 10;
+        }
+        return answer;
+    }
+}
