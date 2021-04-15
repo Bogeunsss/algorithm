b48cfebeb99a99f7343b595daa695a13504284ed
bogeunsss <bogun11904@gmail.com>
2021-04-16 02:36:31 +0900

    기능개발

--- PROGRAMMERS/level_2/README.md
+++ PROGRAMMERS/level_2/README.md
@@ -2,4 +2,5 @@
 
 - [42583](https://programmers.co.kr/learn/courses/30/lessons/42583?language=java) | 다리를 지나는 트럭
 - [42584](https://programmers.co.kr/learn/courses/30/lessons/42584?language=java) | 주식가격
+- [42586](https://programmers.co.kr/learn/courses/30/lessons/42586?language=java) | 기능개발
 - [42587](https://programmers.co.kr/learn/courses/30/lessons/42587?language=java) | 프린터
---
+++ PROGRAMMERS/level_2/_42586/Solution.java
@@ -0,0 +1,40 @@
+package PROGRAMMERS.level_2._42586;
+
+import java.util.*;
+
+public class Solution {
+    public static int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> fin = new ArrayList<Integer>();
+
+        for(int i=0; i<progresses.length; i++) {
+            if((100 - progresses[i]) % speeds[i] == 0) progresses[i] = (100 - progresses[i]) / speeds[i];
+            else progresses[i] = (100 - progresses[i]) / speeds[i] + 1;
+        }
+
+        int j = 0;
+        for(int i=0; i<progresses.length; i++) {
+            int complete = 0;
+            for(j=i+1; j<progresses.length; j++) {
+                if(progresses[i] < progresses[j]) {
+                    i = j - 1;
+                    break;
+                }
+                complete++;
+            }
+            fin.add(complete+1);
+            if(j == progresses.length) break;
+        }
+
+        int[] answer = new int[fin.size()];
+        for(int k=0; k<fin.size(); k++) answer[k] = fin.get(k);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] progresses = {98, 99, 97, 96};
+        int[] speeds = {1, 1, 1, 1};
+
+        System.out.println(Arrays.toString(solution(progresses, speeds)));
+    }
+}
