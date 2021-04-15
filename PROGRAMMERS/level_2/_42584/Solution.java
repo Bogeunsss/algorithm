d0fc638f251255c9e6a4f7e709f1efab8fd6341e
bogeunsss <bogun11904@gmail.com>
2021-04-16 00:55:28 +0900

    주식가격

--- PROGRAMMERS/level_2/_42584/Solution.java
+++ PROGRAMMERS/level_2/_42584/Solution.java
@@ -3,15 +3,12 @@ package PROGRAMMERS.level_2._42584;
 public class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        int index = 0;
 
         for(int i=0; i<prices.length; i++) {
-            int seconds = -1;
-            for(int j=i; j<prices.length; j++) {
-                seconds++;
+            for(int j=i+1; j<prices.length; j++) {
+                answer[i]++;
                 if(prices[i] > prices[j]) break;
             }
-            answer[index++] = seconds;
         }
         return answer;
     }
