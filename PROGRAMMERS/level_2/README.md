e9f9ee0f62b897f57d4a0900ca4d1781de18cfa7
bogeunsss <bogun11904@gmail.com>
2021-04-16 00:33:57 +0900

    프린터

--- PROGRAMMERS/level_2/README.md
+++ PROGRAMMERS/level_2/README.md
@@ -1,3 +1,4 @@
 # Problems
 
 - [42583](https://programmers.co.kr/learn/courses/30/lessons/42583?language=java) | 다리를 지나는 트럭
+- [42587](https://programmers.co.kr/learn/courses/30/lessons/42587?language=java) | 프린터
---
+++ PROGRAMMERS/level_2/_42587/Solution.java
@@ -0,0 +1,44 @@
+package PROGRAMMERS.level_2._42587;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+    public static class Work {
+        int priority;
+        int location;
+
+        public Work(int priority, int location) {
+            this.priority = priority;
+            this.location = location;
+        }
+    }
+    public static int solution(int[] priorities, int location) {
+        Queue<Work> works = new LinkedList<Work>();
+        int[] finished = new int[priorities.length];
+        int index = 0;
+
+        for(int i=0; i<priorities.length; i++) {
+            works.offer(new Work(priorities[i], i));
+        }
+        Arrays.sort(priorities);
+        while(!works.isEmpty()) {
+            Work work = works.poll();
+            if(work.priority == priorities[priorities.length-index-1]) {
+                if(work.location == location) return index+1;
+                index++;
+            }else {
+                works.offer(work);
+            }
+        }
+        return priorities.length;
+    }
+
+    public static void main(String[] args) {
+        int[] priorities = {1, 1, 9, 1, 1, 1};
+        int location = 0;
+
+        System.out.println(solution(priorities, location));
+    }
+}
