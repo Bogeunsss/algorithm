2e1ebaac6568c6153bd999a0135292ffaeb92307
bogeunsss <bogun11904@gmail.com>
2021-05-23 23:42:11 +0900

    정수 삼각형

---
+++ PROGRAMMERS/level_3/_43105/Solution.java
@@ -0,0 +1,12 @@
+package PROGRAMMERS.level_3._43105;
+
+public class Solution {
+    public int solution(int[][] triangle) {
+        for(int i=triangle.length-1; i>0; i--) {
+            for(int j=0; j<triangle[i].length-1; j++) {
+                triangle[i-1][j] += Math.max(triangle[i][j], triangle[i][j+1]);
+            }
+        }
+        return triangle[0][0];
+    }
+}
