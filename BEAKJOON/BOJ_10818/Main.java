176c1ffff9a4f3d2f912d9e8f79aff170fb58796
bogeunsss <bogun11904@gmail.com>
2021-06-20 02:21:07 +0900

    최소, 최대

--- BEAKJOON/BOJ_10818/Main.java
+++ BEAKJOON/BOJ_10818/Main.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class BOJ_10818 {
+public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
