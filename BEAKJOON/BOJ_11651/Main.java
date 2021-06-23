3985234f15d7bd798260069254ecb1de54bda443
bogeunsss <bogun11904@gmail.com>
2021-06-23 18:56:08 +0900

    좌표 정렬하기 2

---
+++ BEAKJOON/BOJ_11651/Main.java
@@ -0,0 +1,35 @@
+package BEAKJOON.BOJ_11651;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) return o1[0] - o2[0];
+                return o1[1] - o2[1];
+            }
+        });
+
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            pq.offer(new int[]{x, y});
+        }
+
+        while(!pq.isEmpty()) {
+            int[] node = pq.poll();
+            System.out.println(node[0] + " " + node[1]);
+        }
+    }
+}
