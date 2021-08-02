6d729f3867c9c9b9ef3cfc1500b0763398eb910d
bogeunsss <bogun11904@gmail.com>
2021-08-02 17:16:38 +0900

    1주차

--- PROGRAMMERS/level_1/_82612/Solution.java
+++ PROGRAMMERS/level_1/_82612/Solution.java
@@ -1,7 +1,7 @@
 package PROGRAMMERS.level_1._82612;
 
 public class Solution {
-    public static long solution(int price, int money, int count) {
+    public long solution(int price, int money, int count) {
         long answer = 0;
 
         for(int i=1; i<=count; i++) {
@@ -9,9 +9,5 @@ public class Solution {
         }
 
         return (money - answer) >= 0 ? 0 : answer - money;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(solution(3, 20, 4));
     }
 }
