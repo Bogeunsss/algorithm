790338eb1202e9cd3931ab26c049eb765bbd3344
bogeunsss <bogun11904@gmail.com>
2021-08-21 22:54:55 +0900

    야근 지수

---
+++ PROGRAMMERS/level_3/_12927/Solution.java
@@ -0,0 +1,37 @@
+package PROGRAMMERS.level_3._12927;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    public static long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+
+        for(int i=0; i<works.length; i++) pq.offer(works[i]);
+        for(int i=0; i<n; i++) {
+            if(!pq.isEmpty() && pq.peek() > 0) {
+                int process = pq.poll();
+                pq.offer(--process);
+            }
+        }
+        while(!pq.isEmpty()) {
+            int work = pq.poll();
+            answer += (long) work * work;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(4, new int[]{4,3,3}));
+        System.out.println(solution(1, new int[]{2,1,2}));
+        System.out.println(solution(3, new int[]{1,1}));
+    }
+}
