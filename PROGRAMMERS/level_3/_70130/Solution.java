aa4d1f405c331c4ef9a67d0a0bc2e136d1311c0b
bogeunsss <bogun11904@gmail.com>
2021-08-29 02:00:28 +0900

    스타 수열

--- PROGRAMMERS/level_3/_70130/Solution.java
+++ PROGRAMMERS/level_3/_70130/Solution.java
@@ -1,12 +1,30 @@
 package PROGRAMMERS.level_3._70130;
 
-public class Solution {
-    public static int solution(int[] a) {
-        int answer = -1;
-        return answer;
-    }
+import java.util.HashMap;
+import java.util.Map;
 
-    public static void main(String[] args) {
-        
+public class Solution {
+    public int solution(int[] a) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int answer = -1;
+
+        for(int n : a) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        for(int key : map.keySet()) {
+            if(map.get(key) <= answer) continue;
+            int count = 0;
+
+            for(int i=0; i<a.length-1; i++) {
+                if(a[i] != key && a[i+1] != key) continue;
+                if(a[i] == a[i+1]) continue;
+                count++;
+                i++;
+            }
+            answer = Math.max(answer, count);
+        }
+
+        return answer * 2;
     }
 }
