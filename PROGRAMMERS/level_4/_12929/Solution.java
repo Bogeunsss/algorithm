3d66f6eba8dad4bf0c3e6ab2d26bfd27e7d4d474
bogeunsss <bogun11904@gmail.com>
2021-08-31 02:16:06 +0900

    올바른 괄호의 갯수

--- PROGRAMMERS/level_4/_12929/Solution.java
+++ PROGRAMMERS/level_4/_12929/Solution.java
@@ -2,7 +2,7 @@ package PROGRAMMERS.level_4._12929;
 
 public class Solution {
 
-    private long catalan(int n) {
+    public int solution(int n) {
         if(n <= 1) return 1;
 
         long x = 1, y = 1;
@@ -10,10 +10,6 @@ public class Solution {
             if(i < n + 1) y *= i;
             else if(i > n + 1) x *= i;
         }
-        return x / y;
-    }
-
-    public int solution(int n) {
-        return (int) catalan(n);
+        return (int) (x / y);
     }
 }
