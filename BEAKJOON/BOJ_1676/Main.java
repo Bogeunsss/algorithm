2b373cc83cdf3b2ce5a5e879ca09220405c17464
bogeunsss <bogun11904@gmail.com>
2021-09-08 23:54:52 +0900

    팩토리얼 0의 개수

--- BEAKJOON/BOJ_1676/Main.java
+++ BEAKJOON/BOJ_1676/Main.java
@@ -6,27 +6,10 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static int count(int n) {
-        int ret = 0;
-
-        while(n > 0) {
-            if(n % 5 == 0) ret++;
-            n /= 5;
-        }
-
-        return ret;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] fives = new int[501];
 
-        for(int i=1; i<=N; i++) {
-            System.out.println(count(i));
-            fives[i] = fives[i-1] + count(i);
-        }
-
-        System.out.println(fives[N]);
+        System.out.println(N / 5 + N / 25 + N / 125);
     }
 }
