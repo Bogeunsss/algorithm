a1e9775efb0068ada5de0f1ed739611272639c5f
bogeunsss <bogun11904@gmail.com>
2021-09-20 00:31:41 +0900

    그대로 출력하기

---
+++ BEAKJOON/BOJ_11718/Main.java
@@ -0,0 +1,20 @@
+package BEAKJOON.BOJ_11718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String s = br.readLine();
+
+            if(s == null) break;
+
+            System.out.println(s);
+        }
+    }
+}
---
+++ BEAKJOON/BOJ_11718/README.md
@@ -0,0 +1,3 @@
+# Problems
+
+- 11718 | 그대로 출력하기 (https://www.acmicpc.net/problem/11718)
