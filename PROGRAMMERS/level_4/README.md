161ccf10a68dbe2eb1b63e9587bddcdb72fe3d14
bogeunsss <bogun11904@gmail.com>
2021-11-25 18:21:18 +0900

    징검다리

--- PROGRAMMERS/level_4/README.md
+++ PROGRAMMERS/level_4/README.md
@@ -2,3 +2,4 @@
 
 - [12902](https://programmers.co.kr/learn/courses/30/lessons/12902?language=java) | 3 x n 타일링
 - [12929](https://programmers.co.kr/learn/courses/30/lessons/12929?language=java) | 올바른 괄호의 갯수
+- [43236](https://programmers.co.kr/learn/courses/30/lessons/43236?language=java) | 징검다리
---
+++ PROGRAMMERS/level_4/_43236/Solution.java
@@ -0,0 +1,39 @@
+package PROGRAMMERS.level_4._43236;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        int low = 1, high = distance;
+        int mid;
+
+        Arrays.sort(rocks);
+
+        while(low <= high) {
+            int prev = 0;
+            int cnt = 0;
+            mid = (high + low) / 2;
+
+            for(int rock : rocks) {
+                if(mid > rock - prev) cnt++;
+                else prev = rock;
+            }
+
+            if(distance - prev < mid) cnt++;
+
+            if(cnt <= n) {
+                low = mid + 1;
+                answer = Math.max(answer, mid);
+            }else high = mid - 1;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(new Solution().solution(25, new int[]{2, 14, 11, 21, 17}, 2));
+    }
+}
